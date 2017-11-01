8bb548f3b69b76bdea359a2c056a91d91713035a
vasil <vasil.sokolov@gmail.com>
2017-11-01 15:30:21 +0300

    update travis config

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ gulp js:build
 gulp js:min
 gulp test
 
-zip -r package.zip data/settings/modules modules static system vendor  ".htaccess" dav.php common.php index.php LICENSE VERSION README.md favicon.ico robots.txt composer.json modules.json gulpfile.js pre-config.json -x **/*.bak
+zip -r ${PRODUCT_NAME}.zip data/settings/modules modules static system vendor  ".htaccess" dav.php index.php LICENSE VERSION README.md favicon.ico robots.txt composer.json modules.json gulpfile.js pre-config.json -x **/*.bak *.git*
