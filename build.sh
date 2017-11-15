37820fbd974c91ead0e772b0c19210306eaffcb0
vasil <vasil.sokolov@gmail.com>
2017-11-15 14:25:59 +0300

    included dev folder to resulting zip

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ gulp js:build
 gulp js:min
 gulp test
 
-zip -r ${PRODUCT_NAME}.zip data/settings/modules modules static system vendor  ".htaccess" dav.php index.php LICENSE VERSION README.md favicon.ico robots.txt composer.json modules.json gulpfile.js pre-config.json -x **/*.bak *.git*
+zip -r ${PRODUCT_NAME}.zip data/settings/modules modules static system vendor dev ".htaccess" dav.php index.php LICENSE VERSION README.md favicon.ico robots.txt composer.json modules.json gulpfile.js pre-config.json -x **/*.bak *.git*
