ae1685ad5538d60dd87c62d6e0dd10ced749ea18
vasil <vasil.sokolov@gmail.com>
2018-02-07 16:53:55 +0300

    removed version override

--- build.sh
+++ build.sh
@@ -7,7 +7,4 @@ gulp js:build
 gulp js:min
 gulp test
 
-now=$(date +"%Y-%m-%d")
-echo $now > VERSION
-
 zip -r ${PRODUCT_NAME}.zip data/settings/modules modules static system vendor dev ".htaccess" dav.php index.php LICENSE VERSION README.md favicon.ico robots.txt composer.json modules.json gulpfile.js pre-config.json -x **/*.bak *.git*
