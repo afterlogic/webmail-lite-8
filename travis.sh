01ab345f3a4e4e86754a9e49aa99e9b2629a6753
vasil <vasil.sokolov@gmail.com>
2018-03-28 15:21:03 +0300

    updated travis builder script

--- travis.sh
+++ travis.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 TASK="build"
-PRODUCT_VERSION=`cat VERSION`
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -28,22 +27,23 @@ if [ "$TASK" = "build" ]; then
 	npm install -g gulp-cli
 	npm install ./modules/CoreWebclient
 
-	gulp styles --themes Default,DeepForest,Funny
-	gulp js:build
-	gulp js:min
+	gulp styles --themes Default,DeepForest,Funny --build a
+	gulp js:build --build a
+	gulp js:min --build a
 	gulp test
+	
+	PRODUCT_VERSION=`cat VERSION`
 	
 	echo CREATE ZIP FILE  = "${PRODUCT_NAME}_${PRODUCT_VERSION}.zip"
 	
 	zip -r ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip data/settings/modules modules static system vendor dev ".htaccess" dav.php index.php LICENSE VERSION README.md favicon.ico robots.txt composer.json modules.json gulpfile.js pre-config.json -x **/*.bak *.git*
-	
 fi
 
 if [ "$TASK" = "upload" ]; then
+	PRODUCT_VERSION=`cat VERSION`
 
 	echo UPLOAD ZIP FILE  = "${PRODUCT_NAME}_${PRODUCT_VERSION}.zip"
 	
 	curl --ftp-create-dirs -T ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip -u ${FTP_USER}:${FTP_PASSWORD} ftp://afterlogic.com/
-	
 fi
 
