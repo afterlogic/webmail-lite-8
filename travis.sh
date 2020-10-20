3910a0ff8f38bfc5225ed8af8bf6a33d27b110bf
nadine-shi <nadinshi@gmail.com>
2020-10-20 09:58:56 +0300

    Improve the data folder protection and update corresponding documentation article

--- travis.sh
+++ travis.sh
@@ -40,12 +40,14 @@ if [ "$TASK" = "build" ]; then
 fi
 	
 if [ "$TASK" = "pack" ]; then
+
+	echo 'deny from all' > data/.htaccess
 	
 	PRODUCT_VERSION=`cat VERSION`
 	
 	echo CREATE ZIP FILE: "${PRODUCT_NAME}_${PRODUCT_VERSION}.zip"
 	
-	zip -rq ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip data/settings/config.json data/settings/modules modules static system vendor dev ".htaccess" dav.php index.php LICENSE VERSION README.md CHANGELOG.txt favicon.ico robots.txt package.json composer.json composer.lock modules.json gulpfile.js pre-config.json -x **/*.bak *.git*
+	zip -rq ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip data/settings/config.json data/settings/modules data/.htaccess modules static system vendor dev ".htaccess" dav.php index.php LICENSE VERSION README.md CHANGELOG.txt favicon.ico robots.txt package.json composer.json composer.lock modules.json gulpfile.js pre-config.json -x **/*.bak *.git*
 fi
 
 if [ "$TASK" = "upload" ]; then
