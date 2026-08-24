cada85c2699006e2a8a1895af0968d1568560d54
Vasil Sokolov <vasil.sokolov@gmail.com>
2026-08-24 14:43:33 +0200

    Updated builder.sh

--- builder.sh
+++ builder.sh
@@ -7,6 +7,7 @@ NC='\033[0m' # No Color
 
 DIR=$(cd `dirname $0` && pwd)
 DIR_VUE="${DIR}/modules/AdminPanelWebclient/vue"
+DIR_VUE_MOBILE="${DIR}/modules/CoreMobileWebclient/vue-mobile"
 
 TASK="list"
 
@@ -41,6 +42,7 @@ if [ "$TASK" = "list" ]; then
   - build
     - build-main
     - build-admin
+    - build-mobile
   - watch-js
   - watch-styles
   - pack
@@ -61,13 +63,18 @@ if [ "$TASK" = "npm" ]; then
 	if [ -d "$DIR_VUE" ]; then
 		cd ${DIR_VUE}
 		npm install
-		npm install -g @quasar/cli
+	fi
+
+	if [ -d "$DIR_VUE_MOBILE" ]; then
+		cd ${DIR_VUE_MOBILE}
+		npm install
 	fi
 fi
 
 if [ "$TASK" = "build" ]; then
 	./builder.sh -t build-main
 	./builder.sh -t build-admin
+	./builder.sh -t build-mobile
 fi
 
 if [ "$TASK" = "build-main" ]; then
@@ -81,6 +88,13 @@ fi
 if [ "$TASK" = "build-admin" ]; then
 	if [ -d "$DIR_VUE" ]; then
 		cd ${DIR_VUE}
+		npm run build-production
+	fi
+fi
+
+if [ "$TASK" = "build-mobile" ]; then
+	if [ -d "$DIR_VUE_MOBILE" ]; then
+		cd ${DIR_VUE_MOBILE}
 		npm run build-production
 	fi
 fi
@@ -110,7 +124,7 @@ if [ "$TASK" = "pack" ]; then
 	
 	printf $GREEN"CREATING ZIP FILE: ${RED}${PRODUCT_NAME}_${PRODUCT_VERSION}.zip\n"$NC
 	
-	zip -rq ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip data/settings/config.json data/settings/modules data/.htaccess modules static system vendor dev adminpanel ".htaccess" dav.php index.php LICENSE VERSION README.md CHANGELOG.txt favicon.ico robots.txt package.json composer.json composer.lock gulpfile.js pre-config.json -x **/*.bak *.git* *node_modules/\*
+	zip -rq ${PRODUCT_NAME}_${PRODUCT_VERSION}.zip data/settings/config.json data/settings/modules data/.htaccess modules static system vendor dev adminpanel ".htaccess" dav.php index.php LICENSE VERSION README.md CHANGELOG.txt favicon.ico robots.txt package.json composer.json composer.lock pre-config.json -x **/*.bak *.git* *node_modules/\*
 fi
 
 if [ "$TASK" = "upload" ]; then
@@ -142,7 +156,7 @@ if [ "$TASK" = "prepare-demo" ]; then
 			sed -i '/"afterlogic\/aurora-framework".*/r extra_modules.txt' composer.json
 		fi
 
-		# temporary removing plesk signup module
+		# temporarily removing plesk signup module
 		sed -i '/aurora-module-mail-signup-plesk/d' composer.json
 		
 		php composer.phar update afterlogic/aurora-module-demo-mode-plugin
